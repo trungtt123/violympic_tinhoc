dfb953f2f8b60ed411b48d85857e42affb89ab3e
Trung Tran <bruceleemax111@gmail.com>
2023-03-03 11:19:06 +0700

    reset repo

--- frontend/src/components/etc/constants.js
+++
@@ -1,6 +0,0 @@
-const domain = 'http://localhost:3000';
-const api_url = 'http://localhost:6969'; 
-//const domain = 'https://violympic-tinhoc.web.app';
-//const api_url = 'https://violympic-it.herokuapp.com';
-const adminID = '60b1a8c9ab7db6261c9a6357'
-export {domain, api_url, adminID}  
